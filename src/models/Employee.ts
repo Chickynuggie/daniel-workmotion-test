ca70932235d81f8152575faa8d572532a4a6306a
Daniel Farkas <daniel.farkas.nsc@gmail.com>
2022-02-02 21:49:42 +0100

    added models

---
+++ src/models/Employee.ts
@@ -0,0 +1,10 @@
+import Employeestate from './employeeStates';
+
+interface Employee {
+  id: Number,
+  firstName: String,
+  lastName: String,
+  status: Employeestate
+}
+
+export default Employee;
---
+++ src/models/EmployeeStates.ts
@@ -0,0 +1,3 @@
+type EmployeeState = "ADDED" | "IN-CHECK" | "APPROVED" | "ACTIVE" | "INACTIVE";
+
+export default EmployeeState;
